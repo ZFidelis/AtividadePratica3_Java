923977895bd6c15d52ca1e0752ed415ceb3d58e4
ZFidelis <isaaczapo@hotmail.com>
2024-05-03 21:04:41 -0300

    Desenvolvedor( ) Constructors, Getters, Setters and Methods

--- AtividadePratica3/src/model/Desenvolvedor.java
+++ AtividadePratica3/src/model/Desenvolvedor.java
@@ -2,4 +2,53 @@ package model;
 
 public class Desenvolvedor extends Funcionario implements Trabalhavel {
     
-}
+    private String linguagemPreferida;
+    private String modeloTrabalho; // Presencial, Hibrido ou Home Office
+
+    //#region Construtores
+    public Desenvolvedor() {
+        
+    }
+
+    public Desenvolvedor(int id, String nome, float valorHora, float horasTrabalhadas, String linguagemPreferida,
+            String modeloTrabalho) {
+        super(id, nome, valorHora, horasTrabalhadas);
+        this.linguagemPreferida = linguagemPreferida;
+        this.modeloTrabalho = modeloTrabalho;
+    }
+    //#endregion
+
+    //#region Getters e Setters
+    public String getLinguagemPreferida() {
+        return linguagemPreferida;
+    }
+
+    public void setLinguagemPreferida(String linguagemPreferida) {
+        this.linguagemPreferida = linguagemPreferida;
+    }
+
+    public String getModeloTrabalho() {
+        return modeloTrabalho;
+    }
+
+    public void setModeloTrabalho(String modeloTrabalho) {
+        this.modeloTrabalho = modeloTrabalho;
+    }
+    //#endregion
+
+    //#region Métodos
+    @Override
+    public float calcularSalario() {
+        float salario = valorHora * horasTrabalhadas;
+        return salario;
+    }
+    //#endregion
+
+    //#region toString
+    @Override
+    public String toString() {
+        String text = super.toString() + "\nLinguagem de Preferencia: " + linguagemPreferida + "\nModelo de Trabalho: " + modeloTrabalho + "\nSalario: " + calcularSalario();
+        return text;
+    }
+    //#endregion
+}   
